fe41ae4070f0218c9801c6c92120de39f5329efb
red2132 <red2132@naver.com>
2025-11-21 16:23:38 +0900

    회원가입, 로그인 구현

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -1,3 +1,65 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in";
+import { useState } from "react";
+import { Link } from "react-router";
+import gitHubLogo from "@/assets/github-mark.svg";
+import { useSignWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
+
 export default function SignInPage() {
-  return <div>Sign in page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { mutate: signInWithPassword } = useSignInWithPassword();
+  const { mutate: signInWithOAth } = useSignWithOAuth();
+
+  const handleSignInPasswordClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+    signInWithPassword({ email, password });
+  };
+
+  const handleSignInWithGithubClick = () => {
+    signInWithOAth("github");
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">로그인</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example.com"
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Button className="w-full" onClick={handleSignInPasswordClick}>
+          로그인
+        </Button>
+        <Button
+          className="w-full"
+          variant={"outline"}
+          onClick={handleSignInWithGithubClick}
+        >
+          <img src={gitHubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
+          계정이 없다면? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
