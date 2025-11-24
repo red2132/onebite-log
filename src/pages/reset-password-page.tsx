a18869b1e5270a1828a769c9045e9a21d816114a
red2132 <red2132@naver.com>
2025-11-24 13:08:44 +0900

    비밀번호 재설정 기능 추가

--- src/pages/reset-password-page.tsx
+++ src/pages/reset-password-page.tsx
@@ -1,3 +1,61 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useUpdatePassword } from "@/hooks/mutations/use-update-password";
+import { generateErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
+
 export default function ResetPasswordPage() {
-  return <div>Reset Password page</div>;
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const { mutate: updatePassword, isPending: isUpdatePasswordPending } =
+    useUpdatePassword({
+      onSuccess: () => {
+        toast.info("비밀번호가 변경되었습니다", {
+          position: "top-center",
+        });
+        navigate("/");
+      },
+
+      onError: (error) => {
+        const message = generateErrorMessage(error);
+        toast.error(message, {
+          position: "top-center",
+        });
+        setPassword("");
+      },
+    });
+
+  const handleUpdatePasswordClick = () => {
+    if (password.trim() === "") return;
+    updatePassword(password);
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-2">
+        <div className="text-xl font-bold">비밀번호 재설정하기</div>
+        <div className="text-muted-foreground">
+          새로운 비밀번호를 입력하세요.
+        </div>
+      </div>
+      <Input
+        className="py-6"
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={isUpdatePasswordPending}
+      />
+      <Button
+        className="w-full"
+        onClick={handleUpdatePasswordClick}
+        disabled={isUpdatePasswordPending}
+      >
+        비밀번호 변경하기
+      </Button>
+    </div>
+  );
 }
