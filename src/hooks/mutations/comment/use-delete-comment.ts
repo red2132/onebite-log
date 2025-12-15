e5f3a3a706bee6c5b65eb3369fe206934c4e3a3c
red2132 <red2132@naver.com>
2025-12-15 17:51:29 +0900

    댓글 캐시 데이터 수정 기능 구현

--- src/hooks/mutations/comment/use-delete-comment.ts
+++ src/hooks/mutations/comment/use-delete-comment.ts
@@ -1,12 +1,25 @@
 import { deleteComment } from "@/api/comment";
-import type { UseMutationCallback } from "@/type";
-import { useMutation } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/lib/constants";
+import type { Comment, UseMutationCallback } from "@/type";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useDeleteComment(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
+    onSuccess: (deletedComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deletedComment.post_id),
+        (comments) => {
+          if (!comments)
+            throw new Error("댓글이 캐시 데이터에 보관되어 있지 않습니다");
+
+          return comments.filter((comment) => comment.id !== deletedComment.id);
+        },
+      );
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
