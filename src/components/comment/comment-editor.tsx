79c9c05cb28249374a022394ed4a8db37cf880e4
red2132 <red2132@naver.com>
2025-12-17 14:13:27 +0900

    대댓글 기능 구현

--- src/components/comment/comment-editor.tsx
+++ src/components/comment/comment-editor.tsx
@@ -17,13 +17,20 @@ type EditMode = {
   onClose: () => void;
 };
 
-type Props = CreateMode | EditMode;
+type ReplyMode = {
+  type: "REPLY";
+  postId: number;
+  parentCommentId: number;
+  onClose: () => void;
+};
+type Props = CreateMode | EditMode | ReplyMode;
 
 export default function CommentEditor(props: Props) {
   const { mutate: createComment, isPending: isCreateCommentPending } =
     useCreateComment({
       onSuccess: () => {
         setContent("");
+        if (props.type === "REPLY") props.onClose();
       },
       onError: (error) => {
         toast.error("댓글 추가에 실패했습니다", {
@@ -60,6 +67,12 @@ export default function CommentEditor(props: Props) {
         postId: props.postId,
         content,
       });
+    } else if (props.type === "REPLY") {
+      createComment({
+        postId: props.postId,
+        content,
+        parentCommentId: props.parentCommentId,
+      });
     } else {
       updateComment({
         id: props.commentId,
@@ -78,15 +91,16 @@ export default function CommentEditor(props: Props) {
         disabled={isPending}
       />
       <div className="flex justify-end gap-2">
-        {props.type === "EDIT" && (
-          <Button
-            disabled={isPending}
-            variant={"outline"}
-            onClick={() => props.onClose()}
-          >
-            취소
-          </Button>
-        )}
+        {props.type === "EDIT" ||
+          (props.type === "REPLY" && (
+            <Button
+              disabled={isPending}
+              variant={"outline"}
+              onClick={() => props.onClose()}
+            >
+              취소
+            </Button>
+          ))}
         <Button onClick={handleSubmitClick} disabled={isPending}>
           작성
         </Button>
--- src/components/comment/commentList.tsx
+++ src/components/comment/commentList.tsx
@@ -2,6 +2,30 @@ import CommentItem from "@/components/comment/comment-item";
 import { useCommentsData } from "@/hooks/queries/use-comments-data";
 import Fallback from "../fallback";
 import Loader from "../loader";
+import type { Comment, NestedComment } from "@/type";
+
+function toNestedComments(comments: Comment[]): NestedComment[] {
+  const result: NestedComment[] = [];
+
+  comments.forEach((comment) => {
+    // 대댓글이 아닌 경우 배열에 댓글 삽입
+    if (!comment.parent_comment_id) {
+      result.push({ ...comment, children: [] });
+    } else {
+      // 대댓글인 경우, 부모 댓글을 찾아 children에 대댓글 삽입
+      const parentCommentIndex = result.findIndex(
+        (item) => item.id === comment.parent_comment_id,
+      );
+
+      result[parentCommentIndex].children.push({
+        ...comment,
+        children: [],
+        parentComment: result[parentCommentIndex],
+      });
+    }
+  });
+  return result;
+}
 
 export default function CommentList({ postId }: { postId: number }) {
   const {
@@ -13,9 +37,11 @@ export default function CommentList({ postId }: { postId: number }) {
   if (fetchCommentsError) return <Fallback />;
   if (isFetchCommentsPending) return <Loader />;
 
+  const nestedComments = toNestedComments(comments);
+
   return (
     <div className="flex flex-col gap-5">
-      {comments.map((comment) => (
+      {nestedComments.map((comment) => (
         <CommentItem key={comment.id} {...comment} />
       ))}
     </div>
